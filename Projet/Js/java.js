babca6ccd900abde680e1cc7ffa32c0b8c9f3f48
Dan FEVRE <alss-sio-sisr21-fda@ccicampus.fr>
2022-03-22 07:41:59 +0100

    sauvegarde après oublie

--- Projet/Js/java.js
+++ Projet/Js/java.js
@@ -34,16 +34,45 @@ select.addEventListener('change', () => {
    }
 })
 
-const eleImage = document.querySelectorAll('.eleImage')
-console.log(eleImage)
 
-eleImage.forEach(element => { element.addEventListener('click', (e) => {
-console.log(e.target.parentNode.innerHTML) 
-   // if (e.target.parentNode )
-})
-   
+
+// Récupération des vignettes (sous forme de tableau)
+const eleImage = document.querySelectorAll(".eleImage");
+// Récupération des images (sous forme de tableau)
+const elemImage2 = document.querySelectorAll(".ShowImage");
+
+// On exécute sur toutes les vignettes le même code : (foreach parcourt le tableau de eleImage)
+eleImage.forEach((element) => {
+  element.addEventListener("click", (e) => {
+    // On récupère le parent de l'endroit où l'on click (donc la vignette)
+    var element = e.target.parentNode;
+    // On test si le click a lieu sur la 2nd div de la vignette
+    if (element.classList.contains("contain")) {
+      //  Si c'est le cas alors on récupère le parent du parent (donc la vignette : a > div > vignette)
+      element = e.target.parentNode.parentNode;
+      //  Si ce n'est pas le cas, element vaut toujours le premier parent de l'élément cliqué
+    } else if (element.classList[1] === undefined) {
+      //  Cette condition s'effectue si l'élément cliqué est la div globale 'vignettes', si c'est le cas elle arrête la fonction.
+      return false;
+    }
+    // On cherche ensuite sur la page l'élément qui a l'id qui correspond à la classe de la vignette
+    var element = document.querySelector("#" + element.classList[1]);
+
+    // On créer une boucle qui fait le tour de toutes les images déjà cachées. Cela pour initialiser la classe hidden pour éviter des problèmes.
+    for (let i = 0; i < elemImage2.length; i++) {
+      //  On ajout donc à toutes les images la classe 'hidden'
+      elemImage2[i].classList.add("hidden");
+    }
+
+    // Ensuite on redirige l'utilisateur avec un href vers la photo
+    window.location.replace("#" + element.id);
+    //   On enlève ensuite la classe hidden ce qui fait apparaître la div
+    element.classList.remove("hidden");
+    document.querySelectorAll(".vignettes").forEach((element) => {
+      element.style.display = "none";
+    });
+  });
 });
-
 
 
 
